04604a928f80820101599d28092911c010286812
baschyd <bastianschymik@gmail.com>
2025-10-30 17:56:58 +0100

    Added bubblesort
    + Bubblesort in Main

--- 03/Bubblesort/src/Main.java
+++ 03/Bubblesort/src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+void main(String[] args) {
+    int [] numbers = new int [args.length];
+    for (int i = 0; i < args.length; i++) {
+        numbers[i] = Integer.parseInt(args[i]);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    for (int i = 0; i < numbers.length; i++) {
+        for (int j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] > numbers[j]) {
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
         }
     }
+
+    for (int n : numbers){
+        IO.print(n + " ");
+    }
+
 }
